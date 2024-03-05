617b55af4116c5fbcb9f103006f17c41f55e06a2
RafaelAlexandre007 <rafael.alexandre013@gmail.com>
2024-03-05 20:23:48 -0300

    tuple

--- Module 1/capitulo05.py
+++ Module 1/capitulo05.py
@@ -62,7 +62,46 @@ l2.clear() # clear: limpa a lista, removendo todos os elementos
 print(l2)
 # Métodos que retornam valores e não alteram a lista
 print(l1.index(40)) # index: retorna o índice da primeira ocorrência do elemento print(l1.count(10)) # count: conta as ocorrências do elemento
+l2 = [2, 'a', 5.44, True] 
+print(max(l2))
+idades = [27, 49, 12, 67, 21, 32, 18, 45, 84, 53, 22, 56, 80, 35, 18] 
+print('Maior idade:', max(idades))
 
 # -> tuple - Ordenada e imutável
+tupla = (0, 1, 2, 3, 4) 
+print(tupla)
+# Tupla heterogênea
+tupla2 = (2, 'a', 5.44, True, None) 
+print(tupla2)
+# Tupla vazia
+tupla3 = () 
+print(tupla3)
+# acesso por índices
+print(tupla[0]) 
+print(tupla[3]) 
+print(tupla[-1])
+# acesso por slices
+print(tupla2[1:4]) 
+print(tupla2[-2:]) 
+print(tupla2[:])
+
+t1 = (30, 10, 20)
+t2 = (2, 'a', 5.44, True)
+# Operações de concatenação (+), repetição (*) e filiação (in)
+print(t1 + t2) 
+print(t1 * 3) 
+print(10 in t1)
+# Funções úteis
+print(len(t2)) # len: retorna a quantidade de elementos da tupla 
+print(min(t1)) # min: retorna o menor elemento da tupla 
+print(max(t1)) # max: retorna o maior elemento da tupla 
+print(sum(t1)) # sum: retorna a soma dos elementos da tupla
+# Métodos que retornam valores
+print(t1.index(20)) # index: retorna o índice da primeira ocorrência do elemento 
+print(t2.count('a')) # count: conta as ocorrências do elemento
+
+t1 = (1, 2, 3) 
+t1[0] = 4
+
 # -> set - Não ordenada, mutavel e valores unicos
 # -> dict - Mapeamento chave e valor, não ordenado e mutavel
